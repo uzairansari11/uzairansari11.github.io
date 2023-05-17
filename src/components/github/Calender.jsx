16e7ebaa214358872fa18ce57472a9f0a1f74922
uzairansari11 <uzairans532@gmail.com>
2023-05-18 02:30:21 +0530

    update github

--- src/components/github/Calender.jsx
+++ src/components/github/Calender.jsx
@@ -32,9 +32,9 @@ function Calender() {
 					className="github-calendar-class"
 					transformData={selectLast12Months}
 					username="uzairansari11"
-					color={"red"}
-					blockSize={20}
-					fontSize={20}
+					color={"teal"}
+					blockSize={16}
+					fontSize={18}
 					showColorLegend
 					hideTotalCount
 				>
@@ -44,21 +44,21 @@ function Calender() {
 			<div className="github-stats">
 				<div id="github-streak">
 					<img
-						src="https://github-readme-streak-stats.herokuapp.com?user=uzairansari11&ring=red&currStreakLabel=000000&fire=black&theme=transparent&hide_border=true&stroke=e0eff8"
+						src="https://github-readme-streak-stats.herokuapp.com?user=uzairansari11&theme=transparent&hide_border=true&border_radius=10.3&date_format=%5BY%20%5DM%20j&sideNums=008080&fire=008080&ring=008080&currStreakNum=008080&currStreakLabel=008080&sideLabels=008080&dates=008080&background=a6d2f0"
 						alt="Uzair Stats"
 						id="github-streak-stats"
 					/>
 				</div>
 				<div id="github-language">
 					<img
-						src="https://github-readme-stats.vercel.app/api/top-langs/?username=uzairansari11&layout=compact&langs_count=4&bg_color=e0eff8&hide_border=true"
+						src="https://github-readme-stats.vercel.app/api/top-langs/?username=uzairansari11&layout=compact&langs_count=4&bg_color=a6d2f0&hide_border=true&border_radius=10&text_color=008080&title_color=008080"
 						alt="Uzair Stats"
 						id="github-top-langs"
 					/>
 				</div>
 				<div id="github-streak-status">
 					<img
-						src="https://github-readme-stats.vercel.app/api?username=uzairansari11&theme=black&show_icons=true&bg_color=e0eff8&hide_border=true"
+						src="https://github-readme-stats.vercel.app/api?username=uzairansari11&theme=transparent&show_icons=true&bg_color=a6d2f0&hide_border=true&border_radius=10&text_color=008080&title_color=008080&icon_color=008080"
 						alt="Uzair Stats"
 						id="github-stats-card"
 					/>
