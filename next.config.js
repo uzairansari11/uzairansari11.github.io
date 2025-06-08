c0da03744e8cc41249e8dd8286dc13576ddfe305
Uzair Ansari <uzairansari@Uzairs-MacBook-Air.local>
2025-06-08 12:51:17 +0530

    updated config

--- next.config.js
+++ next.config.js
@@ -1,23 +1,10 @@
-/** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production"
 
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
 
-  // Static export for GitHub Pages
+
   output: "export",
 
-  // Since this is a user page repo served at root, no basePath needed
-  basePath: "",
-  assetPrefix: isProd ? "" : "",
-}
+ 
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
